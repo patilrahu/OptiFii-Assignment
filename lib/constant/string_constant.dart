3bf4662943f761f9e21ae267056d9f231fbd088e
Rahul Patil <rahul.patil@multark.com>
2025-06-07 15:41:59 +0530

    FEAT - Basic Widget Added

---
+++ lib/constant/string_constant.dart
@@ -0,0 +1,5 @@
+import 'package:google_fonts/google_fonts.dart';
+
+class StringConstant {
+  static String appFontFamily = GoogleFonts.lexend().fontFamily!;
+}
---
+++ lib/widgets/app_text.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:remburshiment_app/constant/string_constant.dart';
+
+// ignore: must_be_immutable
+class AppText extends StatefulWidget {
+  String text;
+  final double? fontSize;
+  final String? fontFamily;
+  final FontWeight? fontWeight;
+  final Color? color;
+  final TextAlign? textAlign;
+  final int? maxLines;
+  final TextOverflow? overflow;
+  AppText({
+    super.key,
+    required this.text,
+    this.fontSize,
+    this.fontFamily,
+    this.fontWeight,
+    this.color,
+    this.textAlign,
+    this.maxLines,
+    this.overflow,
+  });
+
+  @override
+  State<AppText> createState() => _AppTextState();
+}
+
+class _AppTextState extends State<AppText> {
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      widget.text,
+      style: TextStyle(
+        fontSize: widget.fontSize,
+        fontFamily: widget.fontFamily ?? StringConstant.appFontFamily,
+        fontWeight: widget.fontWeight,
+        color: widget.color,
+      ),
+      textAlign: widget.textAlign,
+      maxLines: widget.maxLines,
+      overflow: widget.overflow,
+    );
+  }
+}
